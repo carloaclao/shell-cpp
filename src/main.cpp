8952b211ccc0d86336bbc51a7a1a85464a540fe5
Carlo Aclao <carlo.aclao1@gmail.com>
2026-08-02 11:51:42 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <string>
+#include <unordered_set>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
+  std::string command = "";
+
   std::cout << "$ ";
+  std::cin >> command;
+
+  std::unordered_set<std::string> availableCommands{};
+  if (!availableCommands.contains(command)) {
+    std::cout << command << ": command not found" << std::endl;
+  }
+  
+  return 0;
 }
