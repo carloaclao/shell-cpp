ede903d1ab7e80c192d228aed6b136f52b56128a
Carlo Aclao <carlo.aclao1@gmail.com>
2026-08-02 13:30:31 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,18 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::string command = "";
-
-  std::cout << "$ ";
-  std::cin >> command;
-
   std::unordered_set<std::string> availableCommands{};
-  if (!availableCommands.contains(command)) {
-    std::cout << command << ": command not found" << std::endl;
+  while (true) {
+    std::string command = "";
+
+    std::cout << "$ ";
+    std::cin >> command;
+
+    if (!availableCommands.contains(command)) {
+      std::cout << command << ": command not found" << std::endl;
+    }
   }
+  
   
   return 0;
 }
