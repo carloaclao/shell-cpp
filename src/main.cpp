6d08f84a5891d9bb13058ac88f3fa9817c19f09b
Carlo Aclao <carlo.aclao1@gmail.com>
2026-08-05 23:47:22 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include <string>
-#include <unordered_set>
+#include <functional>
+#include <map>
+
+std::map<std::string, std::function<void()>> availableCommands;
+
+void handleCommand(const std::string command) {
+  auto it = availableCommands.find(command);
+  if (it != availableCommands.end()) {
+    it->second();
+  } else {
+    std::cout << command << ": command not found" << std::endl;
+  }
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::unordered_set<std::string> availableCommands{};
+  availableCommands.emplace("exit", []() { std::exit(EXIT_SUCCESS); });
+
   while (true) {
     std::string command = "";
 
     std::cout << "$ ";
     std::cin >> command;
 
-    if (!availableCommands.contains(command)) {
-      std::cout << command << ": command not found" << std::endl;
-    }
+    handleCommand(command);    
   }
   
   
