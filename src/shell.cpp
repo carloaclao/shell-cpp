90293f7a9a62fe86dd1e4a1b1470d6da2fc06f47
Carlo Aclao <carlo.aclao1@gmail.com>
2026-09-05 23:23:18 -0400

    codecrafters submit [skip ci]

--- src/shell.cpp
+++ src/shell.cpp
@@ -63,7 +63,7 @@ bool Shell::isCommandInPath(const std::string& command, std::string& commandPath
         // checking for execution permissions
         if (((permissions & fs::perms::owner_exec) != fs::perms::none) ||
             ((permissions & fs::perms::group_exec) != fs::perms::none) || 
-            ((permissions & fs::perms::group_exec) != fs::perms::none) 
+            ((permissions & fs::perms::others_exec) != fs::perms::none) 
         ) {
           commandPath = entry.path();
           return true;
